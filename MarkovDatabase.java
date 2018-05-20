e3ce9edc63f66ad3baea405196cfdf75b8e0d83f
mechasoul <35241662+mechasoul@users.noreply.github.com>
2018-05-19 20:25:24 -0700

    fixed indentation

--- MarkovDatabase.java
+++ MarkovDatabase.java
@@ -96,74 +96,74 @@ public class MarkovDatabase {
 		return newestFile;
 	}
 
-    /* take string @in, and go down the list, in 2-word pairs, and record the following word
-     * wordpairs (bigrams) act as keys in master table, map to another hashtable
-     * second hashtable contains words that follow that word pair, w/ the number of times theve been recorded
-     * so like, maybe master record contains pairs "it is", "i am", "that was", etc
-     * then a single pair eg "i am" maps to its own dict, which maps words that have followed the phrase "i am"
-     * so "i am" maps to a dict that maps "nice" -> 6, "cool" -> 3, "gay" -> 1 if we've recorded
-     * "i am nice" 6 times, "i am cool" 3 times, "i am gay" 1 time
-	 * 
-	 * inputs:
-	 * @table: the master databse map to add the pairs to
-	 * @in: the parsed line to be added (parse via InputHandler. starts with start token)
-	 * @starts: 
-	 * 
-	 * outputs:
-	 * stores the data from @in into @table
-	 * increments the count of the starting word in our starting word map
-	 */
+	/* take string @in, and go down the list, in 2-word pairs, and record the following word
+	* wordpairs (bigrams) act as keys in master table, map to another hashtable
+	* second hashtable contains words that follow that word pair, w/ the number of times theve been recorded
+	* so like, maybe master record contains pairs "it is", "i am", "that was", etc
+	* then a single pair eg "i am" maps to its own dict, which maps words that have followed the phrase "i am"
+	* so "i am" maps to a dict that maps "nice" -> 6, "cool" -> 3, "gay" -> 1 if we've recorded
+	* "i am nice" 6 times, "i am cool" 3 times, "i am gay" 1 time
+	* 
+	* inputs:
+	* @table: the master databse map to add the pairs to
+	* @in: the parsed line to be added (parse via InputHandler. starts with start token)
+	* @starts: 
+	* 
+	* outputs:
+	* stores the data from @in into @table
+	* increments the count of the starting word in our starting word map
+	*/
 	public void process(List<String> in) {
 		
 		//special case: line only contains start token (something is horribl wrong)
 		if(in.size() <= 1) {
-    		//dont do shit
+			//dont do shit
 		} else {
 			String firstWord = in.get(1).intern();
 			String nextWord;//to store the word following the bigram
-	    	int currentValue=0;
+			int currentValue=0;
 
-    		//first we add start word to our map. if it's not already there:
-    		if(!startMaster.containsKey(firstWord)) {
-    			startMaster.put(firstWord, 1);//set its count to 1
-    		} else {
-    			currentValue = startMaster.get(firstWord) + 1;
-    			startMaster.put(firstWord, currentValue);//otherwise increment its count
-    		}
-    		
-    		//similarly, create a total counter if its not already there
-    		if(!startMaster.containsKey("<[TOTAL]>".intern())) {
-    			startMaster.put("<[TOTAL]>".intern(), 1);
-    		//otherwise increment it
-    		} else {
-    			currentValue = startMaster.get("<[TOTAL]>".intern()) + 1;
-    			startMaster.put("<[TOTAL]>".intern(), currentValue);
-    		}
-    		
-    		//boot out the oldest start word, add the new one
-    		recentStartQueue.removeLast();
-    		recentStartQueue.addFirst(firstWord);
-    		
-    		for(int i=1; i < in.size(); i++) {
-    			//always exists bc length >=2
-    			Bigram curKey = new Bigram(in.get(i-1).intern(), in.get(i).intern());
-    			ArrayList<String> curList;
+			//first we add start word to our map. if it's not already there:
+			if(!startMaster.containsKey(firstWord)) {
+				startMaster.put(firstWord, 1);//set its count to 1
+			} else {
+				currentValue = startMaster.get(firstWord) + 1;
+				startMaster.put(firstWord, currentValue);//otherwise increment its count
+			}
+			
+			//similarly, create a total counter if its not already there
+			if(!startMaster.containsKey("<[TOTAL]>".intern())) {
+				startMaster.put("<[TOTAL]>".intern(), 1);
+				//otherwise increment it
+			} else {
+				currentValue = startMaster.get("<[TOTAL]>".intern()) + 1;
+				startMaster.put("<[TOTAL]>".intern(), currentValue);
+			}
+			
+			//boot out the oldest start word, add the new one
+			recentStartQueue.removeLast();
+			recentStartQueue.addFirst(firstWord);
+			
+			for(int i=1; i < in.size(); i++) {
+				//always exists bc length >=2
+				Bigram curKey = new Bigram(in.get(i-1).intern(), in.get(i).intern());
+				ArrayList<String> curList;
 
 				if(i==(in.size() - 1)) {
-	    			nextWord = "<[END]>".intern();//if we're at the end of the string, bigram maps to a special end-of-line indicator
-	    		} else {
-	    			nextWord = in.get(i+1).intern();//otherwise it maps to the next word. avoid oob by above
-	    		}
-	    		if(master.containsKey(curKey)) {
-	    			curList = master.get(curKey);//curList is the list of words following the bigram
-	    			
-	    		} else {//first instance of this bigram
-	    			curList = new ArrayList<String>(1);//need to create a new list
-	    		}
-	    		curList.add(nextWord.intern());
-	    		master.put(curKey, curList);//update table for this server
-    		}
-    	}
+					nextWord = "<[END]>".intern();//if we're at the end of the string, bigram maps to a special end-of-line indicator
+				} else {
+					nextWord = in.get(i+1).intern();//otherwise it maps to the next word. avoid oob by above
+				}
+				if(master.containsKey(curKey)) {
+					curList = master.get(curKey);//curList is the list of words following the bigram
+					
+				} else {//first instance of this bigram
+					curList = new ArrayList<String>(1);//need to create a new list
+				}
+				curList.add(nextWord.intern());
+				master.put(curKey, curList);//update table for this server
+			}
+		}
 	}
 	
 	public void loadDatabase() {
@@ -229,7 +229,7 @@ public class MarkovDatabase {
 				LineIterator it = FileUtils.lineIterator(masterFile, "UTF-8");
 				
 				try {
-				
+					
 					//loop through each line in our input file
 					//form the first two words into our key
 					//the remaining words are used to form the list of following words
@@ -261,7 +261,7 @@ public class MarkovDatabase {
 				String test="";
 				
 				try {
-				
+					
 					while(startIt.hasNext()) {
 						String line = startIt.nextLine();
 						test = line;
@@ -285,9 +285,9 @@ public class MarkovDatabase {
 				long time2 = System.currentTimeMillis();
 				try {
 					dir = new File("./logs/" + identifier);
-	    			if(!(dir.isDirectory())) {//if directory doesnt exist
-	    				dir.mkdirs();//create it
-	    			}
+					if(!(dir.isDirectory())) {//if directory doesnt exist
+						dir.mkdirs();//create it
+					}
 					PrintWriter out = new PrintWriter("./logs/" + identifier + "/" + timeStamp + "_debug-load_" + identifier + "-" + name + ".txt");
 					out.println("load finished on " + identifier + " in " + (time2 - time1) + "ms");
 					out.close();
@@ -314,74 +314,74 @@ public class MarkovDatabase {
 			if(!(previousChecksum.equals(currentChecksum))) {
 				previousChecksum = currentChecksum;
 				dir = new File("./markovdb/" + identifier);
-    			if(!(dir.isDirectory())) {//if directory doesnt exist
-    				dir.mkdirs();//create it
-    			}
-    			
-    			StringBuilder builder = new StringBuilder();
-    			//iterate over all entries in our master map
-    			//need to further iterate over all entries in the following list
-    			//our output csv will be formatted as:
-    			//keyword1\\keyword2\\followingword1\\followingword2\\followingword3\\followingword4\\...
-    			for(Entry<Bigram, ArrayList<String>> e : master.entrySet()) {
-    	        	String word1 = e.getKey().getWord1().intern();
-    	        	String word2 = e.getKey().getWord2().intern();
-    	        	
-    	        	ArrayList<String> curList = e.getValue();
-    	        	
-    	        	builder.append(word1 + "||" + word2);
-    	        	
-    	        	for(int i=0; i < curList.size(); i++) {
-    	        		builder.append("||" + curList.get(i).intern());
-    	        	}
-    	        	
-    	        	builder.append("\r\n");
-    	        }
-    			//and shove it all in a csv
-    			String timeStamp = new SimpleDateFormat("yyyyMMdd-HHmmss").format(Calendar.getInstance().getTime());
-    			String data = builder.toString().trim();
-    			long time2 = System.currentTimeMillis();
-    			masterFile = new File("./markovdb/" + identifier + "/" + timeStamp + "_" + identifier + "-" + name + ".masterdb");
-    			try {
-    				FileUtils.writeStringToFile(masterFile, data, utf8);
-    			} catch (IOException e) {
-    				e.printStackTrace();
-    			}
-    			long time3 = System.currentTimeMillis();
-    			StringBuilder startBuilder = new StringBuilder();
-    			//repeat the process for our start master file
-    			for(Entry<String, Integer> e : startMaster.entrySet()) {
-    				startBuilder.append(e.getKey().intern() + "||");
-    				startBuilder.append("" + e.getValue());
-    				startBuilder.append("\r\n");
-    			}
-    			String startData = startBuilder.toString().trim();
-    			long time4 = System.currentTimeMillis();
-    			startFile = new File("./markovdb/" + identifier + "/" + timeStamp + "_start_" + identifier + "-" + name + ".startdb");
-    			try {
-    				FileUtils.writeStringToFile(startFile, startData, utf8);
-    			} catch (IOException e) {
-    				e.printStackTrace();
-    			}
-    			long time5 = System.currentTimeMillis();
-    			if(debugOn) {
-    				try {
-    					dir = new File("./logs/" + identifier);
-    	    			if(!(dir.isDirectory())) {//if directory doesnt exist
-    	    				dir.mkdirs();//create it
-    	    			}
-    					PrintWriter out = new PrintWriter("./logs/" + identifier + "/" + timeStamp + "_debug-save_" + identifier + "-" + name + ".txt");
-    					out.println("save finished on " + identifier + " in " + (time5 - time1) + "ms");
-    					out.println("stringbuilder for master took: " + (time2 - time1) + "ms");
-    					out.println("file write for master took: " + (time3 - time2) + "ms");
-    					out.println("stringbuilder for start took: " + (time4 - time3) + "ms");
-    					out.println("file write for start took: " + (time5 - time4) + "ms");
-    					out.close();
-    				} catch (FileNotFoundException e) {
-    					// TODO Auto-generated catch block
-    					e.printStackTrace();
-    				}
-    			}
+				if(!(dir.isDirectory())) {//if directory doesnt exist
+					dir.mkdirs();//create it
+				}
+				
+				StringBuilder builder = new StringBuilder();
+				//iterate over all entries in our master map
+				//need to further iterate over all entries in the following list
+				//our output csv will be formatted as:
+				//keyword1\\keyword2\\followingword1\\followingword2\\followingword3\\followingword4\\...
+				for(Entry<Bigram, ArrayList<String>> e : master.entrySet()) {
+					String word1 = e.getKey().getWord1().intern();
+					String word2 = e.getKey().getWord2().intern();
+					
+					ArrayList<String> curList = e.getValue();
+					
+					builder.append(word1 + "||" + word2);
+					
+					for(int i=0; i < curList.size(); i++) {
+						builder.append("||" + curList.get(i).intern());
+					}
+					
+					builder.append("\r\n");
+				}
+				//and shove it all in a csv
+				String timeStamp = new SimpleDateFormat("yyyyMMdd-HHmmss").format(Calendar.getInstance().getTime());
+				String data = builder.toString().trim();
+				long time2 = System.currentTimeMillis();
+				masterFile = new File("./markovdb/" + identifier + "/" + timeStamp + "_" + identifier + "-" + name + ".masterdb");
+				try {
+					FileUtils.writeStringToFile(masterFile, data, utf8);
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+				long time3 = System.currentTimeMillis();
+				StringBuilder startBuilder = new StringBuilder();
+				//repeat the process for our start master file
+				for(Entry<String, Integer> e : startMaster.entrySet()) {
+					startBuilder.append(e.getKey().intern() + "||");
+					startBuilder.append("" + e.getValue());
+					startBuilder.append("\r\n");
+				}
+				String startData = startBuilder.toString().trim();
+				long time4 = System.currentTimeMillis();
+				startFile = new File("./markovdb/" + identifier + "/" + timeStamp + "_start_" + identifier + "-" + name + ".startdb");
+				try {
+					FileUtils.writeStringToFile(startFile, startData, utf8);
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+				long time5 = System.currentTimeMillis();
+				if(debugOn) {
+					try {
+						dir = new File("./logs/" + identifier);
+						if(!(dir.isDirectory())) {//if directory doesnt exist
+							dir.mkdirs();//create it
+						}
+						PrintWriter out = new PrintWriter("./logs/" + identifier + "/" + timeStamp + "_debug-save_" + identifier + "-" + name + ".txt");
+						out.println("save finished on " + identifier + " in " + (time5 - time1) + "ms");
+						out.println("stringbuilder for master took: " + (time2 - time1) + "ms");
+						out.println("file write for master took: " + (time3 - time2) + "ms");
+						out.println("stringbuilder for start took: " + (time4 - time3) + "ms");
+						out.println("file write for start took: " + (time5 - time4) + "ms");
+						out.close();
+					} catch (FileNotFoundException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+				}
 			}
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
